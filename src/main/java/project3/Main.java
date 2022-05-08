7921c1c085a1c25976b50b7292c5f78ecf2fa6ec
CansuG <cansugurel@eskisehir.edu.tr>
2022-05-08 16:58:30 +0300

    added an example to main

--- src/main/java/project3/Main.java
+++ src/main/java/project3/Main.java
@@ -8,10 +8,17 @@ public class Main {
         Subscriber customer3=new Subscriber("Jan",lgPublisher);
         System.out.println("LG current state : "+ lgPublisher.getAvailability());
         lgPublisher.setAvailability("Available");
+        System.out.println("-----------------------------------------");
         Publisher applePublisher = new Publisher(Brand.APPLE,10000,"Out Of Stock");
         Subscriber customer4=new Subscriber("Sevval",applePublisher);
         System.out.println("Apple current state : "+ applePublisher.getAvailability());
         applePublisher.setAvailability("Available");
-        System.out.println("");
+        System.out.println("-----------------------------------------");
+        Publisher samsungPusblisher = new Publisher(Brand.SAMSUNG, 5000, "Out of Stock");
+        Subscriber customer5 = new Subscriber("Sevcan", samsungPusblisher);
+        Subscriber customer6 = new Subscriber("Yağmur", samsungPusblisher);
+        Subscriber customer7 = new Subscriber("Can", samsungPusblisher);
+        System.out.println("Samsung current state: "+ samsungPusblisher.getAvailability());
+        samsungPusblisher.setAvailability("Available");
     }
 }
